52540c2cec2d4d275b863712c9736bb1e9b0ec67
Tommy Carpenter <tc677g@att.com>
2020-02-26 14:12:54 -0500

    Initial pass of the py xapp frame
    This is not yet completed:
    - Docs will come soon
    - More unit test coverage will come soon
    - More functionality will come soon
    
    However, to avoid a gargantuan commit at the beginning that solves world
    hunger, we cut off here on a basically usable framework.
    
    NOTE: a significant portion of this code is *already reviewed* as it
    came out of A1. For example, the entire SDLWrapper class, and most of
    the RMRLoop. This code will
    be deleted from A1 when this is in pypi.
    
    Issue-ID: RIC-228
    Change-Id: I9f87b0178cf5fb1cb6b988c29ce11218ed575607
    Signed-off-by: Tommy Carpenter <tc677g@att.com>

---
+++ ricxappframe/xapp_rmr.py
@@ -0,0 +1,88 @@
+"""
+Contains rmr functionality specific to the xapp
+The general rmr API is via "rmr"
+"""
+# ==================================================================================
+#       Copyright (c) 2020 Nokia
+#       Copyright (c) 2020 AT&T Intellectual Property.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#          http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+# ==================================================================================
+
+
+import time
+import queue
+from threading import Thread
+from mdclogpy import Logger
+from rmr import rmr, helpers
+
+
+mdc_logger = Logger(name=__name__)
+
+
+class RmrLoop:
+    """
+    Class represents an rmr loop that constantly reads from rmr
+
+    Note, we use a queue here, and a thread, rather than the xapp frame just looping and calling consume, so that a possibly slow running consume function does not block the reading of new messages
+    """
+
+    def __init__(self, rmr_port):
+        """
+        sets up rmr, then launches a thread that reads and injects messages into a queue
+        """
+
+        # Public
+        # thread safe queue https://docs.python.org/3/library/queue.html
+        # We use a thread and a queue so that a long running consume callback function can never block reads.
+        # IE a consume implementation could take a long time and the ring size for rmr blows up here and messages are lost
+        self.rcv_queue = queue.Queue()
+
+        # rmr context; RMRFL_MTCALL puts RMR into a multithreaded mode, where a thread populates a ring of messages that eceive calls read from
+        mdc_logger.debug("Waiting for rmr to init on port {}..".format(rmr_port))
+        self.mrc = rmr.rmr_init(str(rmr_port).encode(), rmr.RMR_MAX_RCV_BYTES, rmr.RMRFL_MTCALL)
+        while rmr.rmr_ready(self.mrc) == 0:
+            time.sleep(0.1)
+
+        # Private
+        self._last_ran = time.time()
+
+        # start the work loop
+        mdc_logger.debug("Starting loop thread")
+        self._thread = Thread(target=self.loop)
+        self._thread.start()
+
+    def loop(self):
+        """
+        loop forever
+        """
+        mdc_logger.debug("Work loop starting")
+        while True:
+
+            # read our mailbox
+            # TODO: take a flag as to whether RAW is needed or not
+            # RAW allows for RTS however the caller must free, and the caller may not need RTS.
+            # Currently after consuming, callers should do  rmr.rmr_free_msg(sbuf)
+
+            for (msg, sbuf) in helpers.rmr_rcvall_msgs_raw(self.mrc):
+                self.rcv_queue.put((msg, sbuf))
+
+            self._last_ran = time.time()
+
+    def healthcheck(self, seconds=30):
+        """
+        returns a boolean representing whether the rmr loop is healthy, by checking two attributes:
+        1. is it running?,
+        2. is it stuck in a long (> seconds) loop?
+        """
+        return self._thread.is_alive() and ((time.time() - self._last_ran) < seconds)

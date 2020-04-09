673570920f640964afd70346eee27e735007d1c8
Lott, Christopher (cl778h) <cl778h@att.com>
2020-04-09 18:25:19 -0400

    Move wrapped C library to subpackage
    Create new subpackage rmr/rmrclib with the C library loaded via ctypes.
    Add method to get constants from RMR library that recognizes mocks.
    This change allows generation of API documentation by Sphinx autodoc
    via a mock when the .so file is not available, for example at ReadTheDocs.
    
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>
    Change-Id: Idb9bb1d7a534c4142ea0353b5a48d3132e55f6e6

--- docs/conf.py
+++ docs/conf.py
@@ -2,16 +2,22 @@ import os
 import sys
 from docs_conf.conf import *
 
+# autodoc needs this to find the code
 sys.path.insert(0, os.path.abspath("../"))
 
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "numpydoc"]
 
-# dont alphabetically order
+# don't alphabetically order
 autodoc_member_order = "bysource"
 
 linkcheck_ignore = ["http://localhost.*", "http://127.0.0.1.*", "https://gerrit.o-ran-sc.org.*"]
+
+# silence complaints from autodoc gen
 nitpick_ignore = [
             ('py:class', 'ctypes.c_char_p'),
             ('py:class', 'ctypes.c_void_p'),
             ('py:class', 'ricxappframe.rmr.rmr.LP_rmr_mbuf_t'),
             ]
+
+# RMR c library is not available in ReadTheDocs
+autodoc_mock_imports = ['ricxappframe.rmr.rmrclib']

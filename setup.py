e2e3e435babf3aeb8c1bae14ad2a055d02bee38a
Lott, Christopher (cl778h) <cl778h@att.com>
2020-04-22 09:52:23 -0400

    Upgrade to RMR version 3.8.0 from released image
    Bump version to 1.0.2 and add release notes
    
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>
    Change-Id: Ie7b9882106b6f6670312f6fa9187191fce104ab8

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def _long_descr():
 
 setup(
     name="ricxappframe",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(exclude=["tests.*", "tests"]),
     author="Tommy Carpenter, E. Scott Daniels",
     description="Xapp and RMR framework for python",

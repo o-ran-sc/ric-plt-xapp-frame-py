387747739d1c47038ad9649f4e524a26f44713e2
Heewon Park <h_w.park@samsung.com>
2020-10-22 17:36:04 +0900

    Add E2AP package that supports asn1 encoding/decoding function for E2AP.
       - indication
       - subscription request/response
       - control request
    
    Issue-ID : RIC-664
    
    Signed-off-by: Heewon Park <h_w.park@samsung.com>
    Change-Id: Id1bb533dd4cf16006f73c8e6b928cca65a832e4b

---
+++ ricxappframe/xapp_e2ap.py
@@ -0,0 +1,26 @@
+# from ricxappframe.e2ap.asn1 import IndicationMsg, SubResponseMsg, SubRequestMsg, ControlRequestMsg
+from ricxappframe.e2ap import asn1
+
+class Indication(asn1.IndicationMsg):
+    def __init__(self):
+        return
+
+class SubscriptionResponse(asn1.SubResponseMsg):
+    def __init__(self):
+        return
+
+class SubscriptionRequest(asn1.SubRequestMsg):
+    def __init__(self):
+        return
+
+class ControlRequest(asn1.ControlRequestMsg):
+    def __init__(self):
+        return
+
+class ActionDefinition(asn1.ActionDefinition):
+    def __init__(self):
+        return
+
+class SubsequentAction(asn1.SubsequentAction):
+    def __init__(self):
+        return
